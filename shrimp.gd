d024d98967edad586b55c45c72601c6d4ef100de
Tristan Tan <Ta4153Tr0527@pusd.us>
2024-10-29 09:10:25 -0700

    sprint B

---
+++ shrimp.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _on_body_entered(body):
+    if body.is_in_group("Player"):  # Assuming player is in 'Player' group
+        # Inform the parent scene to add score
+        get_parent().add_score(1)  # Notify main scene to increase score
+        queue_free()  # Remove the collectible
